11069367596c359bca02a8fb164014bd2b040686
nguyenkhanh08112003 <nguyenkhanh08112003@gmail.com>
2021-09-27 09:43:21 +0700

    Create vidu.c

---
+++ vidu.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*Fig.2.1: Fig 02_01.c
+ A first programs in C 
+ function main programs execution */
+	int main (void){
+	
+		printf("Welcome to C!\n");
+	
+	return 0; /* indicate that program ended successfully */
+} /* end function main */
