9b94693cff6ba37b3a507badadb27a57e8dec9c6
nguyenkhanh08112003 <nguyenkhanh08112003@gmail.com>
2021-09-29 11:29:48 +0700

    Create bai02.c

---
+++ bai02.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int num=10;
+	char ch = 'A';
+	float f = 25.3;
+	double d = 25.3;
+	printf("num = %d", num);
+	printf("\nch = %c", ch);
+	printf("\nf = %.2f", f);
+	printf("\nd=%f", d);
+	getch();
+}
+// Nhan xet :
+// Ket qua hien thi ra man hinh gia tri cua f voi d tuy giong nhau nhung o gia tri cua d xuat hien nhieu chu so thap phan hon 
+
+
