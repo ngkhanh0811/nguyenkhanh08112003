ea1c0768cf2db2e9a20fb75dfeca28891a0abbd0
nguyenkhanh08112003 <nguyenkhanh08112003@gmail.com>
2021-09-29 11:30:02 +0700

    Create bai08.c

---
+++ bai08.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	printf("Kich thuoc cua cac kieu du lieu : ");
+	printf("\n Kieu int : 4 Bytes ");
+	printf("\n So nguyen : 15 ");
+	printf("\n Kieu float : 4 Bytes ");
+	printf("\n So thuc kieu float : 3.456000 ");
+	printf("\n Kieu double : 8 Bytes ");
+	printf("\n So thuc kieu double : 3.457984598457984");
+	printf("\n Kieu char : 1 Bytes ");
+	printf("\n Ky tu : E ");
+	printf("\n Kieu long int : 4 Bytes ");
+	printf("\n Kieu long double : 12 Bytes ");
+	return 0;
+}
