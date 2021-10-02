87daedee6b9b520d759ba488ec9e27bbc171612f
nguyenkhanh08112003 <nguyenkhanh08112003@gmail.com>
2021-10-02 21:51:07 +0700

    Create vidu1ss7.c

---
+++ vidu1ss7.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+void main(int argc, char *argv[]) {
+	int x, y;
+	char a='y';
+	x=y=0;
+	if (a=='y');
+	{
+		x+=5;
+		printf("Hai so x va y la %d va %d\t", x, y);}
+	return 0;
+}
