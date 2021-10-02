b936f2485aa9164f2359a6f1cb32855c4580d4d4
nguyenkhanh08112003 <nguyenkhanh08112003@gmail.com>
2021-10-02 21:51:13 +0700

    Create vidu3ss7.c

---
+++ vidu3ss7.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+void main(int argc, char *argv[]) {
+	int a, b;
+    printf("Nhap so a : ");
+    scanf("%d", &a);
+    if(b==a%2)
+    printf("a la so chan ");
+    else 
+    printf("a la so le ");
+	return 0;
+}
